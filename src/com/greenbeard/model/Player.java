26b39da33727dd9554262353591e73c04624256e
redrianhood <rian.boren@gmail.com>
2022-01-26 14:10:44 -0800

    outline player and character classes

--- src/com/greenbeard/model/Player.java
+++ src/com/greenbeard/model/Player.java
@@ -1,5 +1,40 @@
 package com.greenbeard.model;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player extends Character{
+    ArrayList<String> crewMates = new ArrayList<>();
+    private int gold = 100;
+
+    public Player() {
+
+    }
+
+    public ArrayList<String> addCrewMate(String name){
+        crewMates.add(name);
+        return crewMates;
+    }
+
+    public ArrayList<String> getCrewMates() {
+        return crewMates;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + getName() + '\'' +
+                ", health=" + getHealth() +
+                ", weapon='" + getWeapon() + '\'' +
+                "crewMates=" + getCrewMates() +
+                ", gold=" + getGold() +
+                '}';
+    }
 }
