26bf920600e4eb2d170ee1364c997bcb6c3025c6
Mark Duenas <markgdlduenas@gmail.com>
2022-01-31 09:35:54 -0800

    merging with subash chagnes

--- src/com/greenbeard/controller/Game.java
+++ src/com/greenbeard/controller/Game.java
@@ -11,9 +11,13 @@ import org.json.simple.parser.ParseException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.Control;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -27,9 +31,18 @@ public class Game {
     private Prompter prompter = new Prompter(new Scanner(System.in));
     private String currentLocation = "town";
     private JSONParser jsonParser = new JSONParser();
+<<<<<<< HEAD
     private boolean dialogue = false;
+=======
+    private Scanner scanner = new Scanner(System.in);
+    private Clip clip;
+    private static long BANNER_DELAY = 1500;
+    private static final long WORD_DELAY = 250;
+
+>>>>>>> 3db1992354c16b6379b8431b45f049c784fbe5e4
 
     public void execute() {
+        gameOver = false;
         welcome();
         while (!gameOver) {
             if (player.getHealth() <= 0) {
@@ -45,8 +58,10 @@ public class Game {
         try {
             File audioFile = new File(fileName);
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-            Clip clip = AudioSystem.getClip();
+            clip = AudioSystem.getClip();
             clip.open(audioStream);
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(-10.0f);
             clip.start();
         } catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
@@ -56,21 +71,28 @@ public class Game {
             e.printStackTrace();
         }
     }
+//    private float getCurrentVolume(Clip clip){
+//        FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+//        return (float) Math.pow(10f, volumeControl.getValue() / 20f);
+//    }
 
     private void welcome() {
-        audio("data/audio/intro.wav");
+        audio("data/audio/gamemusic.wav");
         System.out.println("\n\n");
         try {
             //Files.lines(Path.of("data/welcome/banner.txt")).forEach(System.out::println);
             Files.lines(Path.of("data/welcome/banner1.txt")).forEach(System.out::println);
-            delay();
+            delay(BANNER_DELAY);
             Files.lines(Path.of("data/welcome/banner2.txt")).forEach(System.out::println);
-            delay();
+            delay(BANNER_DELAY);
             Files.lines(Path.of("data/welcome/banner3.txt")).forEach(System.out::println);
-            delay();
+            delay(BANNER_DELAY);
+
             List<String> welcome = Files.readAllLines(Path.of("data/welcome/welcome.txt"));
             List<String> intro = Files.readAllLines(Path.of("data/welcome/intro.txt"));
-            welcome.forEach(System.out::println);
+            welcome.forEach((line) -> {
+                printWordByWord(line);
+            });
             player.setName(prompter.prompt("\nWhat is your name Captain? -> "));
             player.setShipName(prompter.prompt("What is the name of your Ship? -> "));
             String weapon = prompter.prompt("What kind of weapon do you carry?\n" +
@@ -79,11 +101,29 @@ public class Game {
             System.out.printf("\n\nYou are the Great Captain %s, Captain of the %s.\n" +
                             "With your trusty %s by your side, you set off to town.%n",
                     player.getName(), player.getShipName(), player.getWeapon());
-            intro.forEach(line -> System.out.println(line));
+            intro.forEach((line) -> {
+                printWordByWord(line);
+            });
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void printWordByWord(String line) {
+        if (!line.trim().isEmpty()) {
+            String[] words = line.split(" ");
+            if ((words == null) || (words.length < 1)) {
+                return;
+            }
+            List<String> wordsList = Arrays.asList(words);
+            wordsList.forEach((word) -> {
+                System.out.print(word + " ");
+                delay(WORD_DELAY);
+            });
+        }
+        System.out.println();
+        delay(WORD_DELAY);
     }
 
     private void clearConsole() {
@@ -91,9 +131,9 @@ public class Game {
         System.out.flush();
     }
 
-    private void delay() {
+    private void delay(long millis) {
         try {
-            Thread.sleep(1500);
+            Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -101,7 +141,7 @@ public class Game {
 
     private void start() {
         String input = prompter.prompt("What would you like to do?\n -> ").toLowerCase();
-        if( !this.currentLocation.equals("town")) {
+        if (!this.currentLocation.equals("town")) {
             System.out.println("Interact with the environment with verb commands, such as 'go', 'talk', 'recruit', followed by a noun.");
         }
         textParser(input);
@@ -123,11 +163,27 @@ public class Game {
             JSONObject location = (JSONObject) jObj.get(this.currentLocation);
 
             if ("go".equals(verb)) {
-                this.currentLocation = noun;
-                location = (JSONObject) jObj.get(this.currentLocation);
+                //check if valid route based on json locations for the current location
+                if (!validateRoute(noun)) {
+                    //invalid route.
+                    System.out.println("Can not go to " + noun + " from " + this.currentLocation);
+                    return;
+                }
+
+                //Valid route.
+
+                //Get the JSON object for the target destinationS
+                location = (JSONObject) jObj.get(noun);
+                //Check if JSON object found for the target destination
                 if (location != null) {
+                    //JSON object found for the target destination
                     String description = (String) location.get("description");
                     System.out.println(description);
+                    this.currentLocation = noun;
+                } else {
+                    //JSON object NOT found for the target destination
+                    System.out.println("No JSON entry for: " + noun);
+                    return;
                 }
             }
             if (!currentLocation.equals("town") && "recruit".equals(verb)) {
@@ -159,6 +215,30 @@ public class Game {
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean validateRoute(String destination) {
+        try (Reader reader = new FileReader("data/locations/locations.json")) {
+            //Get the JSON Data for the current location
+            JSONObject jObj = (JSONObject) jsonParser.parse(reader);
+            JSONObject currentLocationJObj = (JSONObject) jObj.get(this.currentLocation);
+//            Get the possible destinations from the current location
+            JSONArray locationsArray = (JSONArray) currentLocationJObj.get("locations");
+
+            //check if the target destination is found in the permitted destinations
+            for (Object locElement : locationsArray) {
+                String locationName = (String) locElement;
+
+                if (locationName.equals(destination)) {
+                    //valid destination
+                    return true;
+                }
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        //destination not found
+        return false;
     }
 
     private String checkSynonym(String command) {
@@ -227,15 +307,18 @@ public class Game {
 
     private void gameOver() {
         System.out.println("GAME OVER!");
-        audio("data/audio/gameover.wav");
         String playAgain = prompter.prompt("Play again? yes or no?\n -> ", "yes|no", "Invalid Choice");
         if ("yes".equals(playAgain)) {
+            resetGame();
             execute();
         } else if ("no".equals(playAgain)) {
             System.out.println("Thanks for playing! See you again!");
-            audio("data/audio/victory.wav");
             System.exit(0);
         }
+    }
+
+    private void resetGame() {
+        player.clearCrewMates();
     }
 
     private void ending() {
